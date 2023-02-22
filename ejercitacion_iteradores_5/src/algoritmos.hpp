a5d00499d40121ce2b15b5450f271a6d27078ec8
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-22 20:03:15 -0300

    Implementacion de las funciones hasta el ejercicio 6

--- ejercitacion_iteradores_5/src/algoritmos.hpp
+++ ejercitacion_iteradores_5/src/algoritmos.hpp
@@ -65,13 +65,58 @@ typename Iterator::value_type promedioIter(const Iterator& desde, const Iterator
 
 }
 
-
-
 /* Ejercicio 4 */
+template<class Contenedor>
+void filtrar(Contenedor& c, const typename Contenedor::value_type& elem){
+    typename Contenedor::const_iterator it = c.begin();
+    while(it != c.end()){
+        if(*it == elem){
+            it = c.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
 
 /* Ejercicio 5 */
+template<class Contenedor>
+bool ordenado(Contenedor& c){
+    auto itElemActual = c.begin();
+    auto itSigElemActual = ++c.begin();
+    while((itSigElemActual != c.end()) && (*itElemActual < *itSigElemActual)) {
+        ++itElemActual;
+        ++itSigElemActual;
+    }
+
+    if(itSigElemActual != c.end()){
+        return false;
+    } else {
+        return true;
+    }
+}
 
 /* Ejercicio 6 */
+template<class Contenedor>
+std::pair<Contenedor,Contenedor> split(const Contenedor& c, const typename Contenedor::value_type& elem){
+    Contenedor menores;
+    Contenedor mayoresIguales;
+    auto itMenores = menores.begin();
+    auto itMayoresIguales = mayoresIguales.begin();
+
+    for(auto it = c.begin(); it != c.end(); ++it){
+        if(*it < elem){
+            /* Insertar en el contenedor de menores */
+            itMenores = menores.insert(itMenores,*it);
+            ++itMenores;
+        } else {
+            /* Insertar en el contenedor de mayores */
+            itMayoresIguales = mayoresIguales.insert(itMayoresIguales,*it);
+            ++itMayoresIguales;
+        }
+    }
+    std::pair<Contenedor,Contenedor> menoresyMayores = std::make_pair(menores,mayoresIguales);
+    return menoresyMayores;
+}
 
 /* Ejercicio 7 */
 
